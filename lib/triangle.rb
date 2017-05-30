e254ad70041527ddc5589d6fc567f43875c497b5
DV <github email address>
2017-05-30 16:07:04 +0900

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+
+  def kind()
+    self.validate()
+
+    if (@sides[0] == @sides[1] && @sides[0] == @sides[2])
+      return :equilateral
+    elsif (@sides[0] == @sides[1] || @sides[0] == @sides[2] || @sides[1] == @sides[2])
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def validate()
+    @sides.each{|side| raise TriangleError if side <= 0}
+    if (@sides[0] + @sides[1] <=  @sides[2] ||
+        @sides[0] + @sides[2] <=  @sides[1] ||
+        @sides[1] + @sides[2] <=  @sides[0])
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Invalid Triangle parameters have been submitted!"
+  end
 end
